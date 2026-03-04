4769bddcfe842589933adb2012a34a9459cbd04a
Jamieson Mansker <jmanske1@eecs.wsu.edu>
2026-03-03 18:00:10 -0800

    Init commit for first exercise. Contains template for MVC project.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PartyInvites.Controllers;
+public class HomeController : Controller
+{
+  
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
